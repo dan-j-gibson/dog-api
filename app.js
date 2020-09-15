213125a6720f648f6e0496d807b2de742c239eea
Dan Gibson <dgibson1124@gmail.com>
2020-09-15 11:18:50 -0700

    final draft

--- app.js
+++ app.js
@@ -1,95 +1,50 @@
-// Create an app that lets users choose to display between 1 and 50 random dog images,
-//  then prints the results to the console. The app should feature a form with a required 
-//  input where users indicate the number of images to retrieve, and the input should default
-//   to 3. Use the endpoint described in the "DISPLAY MULTIPLE RANDOM IMAGES FROM ALL DOGS 
-//   COLLECTION" section of this page of the DogAPI docs.
-
-
-// return random image if valid and error message of no connection
-'use strict';
-
-// proposed finished version
-function getDogImages(query, displayCallback) {
-  fetch(`https://dog.ceo/api/breeds/image/random/${query}`)
+// fetch images from dog API 
+function getImages(query, callback) {
+  if (query <=50){fetch(`https://dog.ceo/api/breeds/image/random/${query}`)
  .then(response => response.json())
  .then(responseJson => {
    console.log(responseJson)
    return responseJson
  }) 
- .then(responseJson => displayCallback(responseJson))
+ .then(responseJson => callback(responseJson))
  .catch(error => alert('Something went wrong. Try again later.'));
 }
-
-
-// original version
-function getImage(userNumber){
-  if (userNumber < 3) {
-    fetch("https://dog.ceo/api/breeds/image/random/3")
-      .then(response => response.json())
-      .then(responseJson => displayResults(responseJson));
-  } else if (userNumber > 50) {
-    return alert("Please choose a number equal or less than 50");
-  } else {
-    fetch(`https://dog.ceo/api/breeds/image/random/${userNumber}`)
-      .then(response => response.json())
-      .then(responseJson => console.log(responseJson));
-  }
+else {
+  return alert('please enter a number under 50')
 }
-// $('.choose-number-form').submit(event =>{
-// event.preventDefault();
-// let numberOfDogs = $("#num-dog").val()};
-
-// getImage(numberOfDogs)
-// );
-
-function watchNumberInput() {
-  $(".choose-number-form").submit(event => {
-    event.preventDefault();
-    let numberOfDogs = $(".num-dog").val();
-    //Pass the number value to getDogImage
-    getImage(numberOfDogs);
-  });
 }
 
 
+function displayResults(responseJson) {
+ return `
+ <div class="img-containter">
+ <h5>Check Out This Pupper</h5>
+   <img src="${responseJson}" class="results-img">
+ </div>
+ ` ;
+}
+
+function searchData(data) {
+ const results = data.message.map((item, index) => displayResults(item));
+ $('.js-results').html(results);
+ 
+ $('.results').removeClass('hidden');
+}
 
 
-
-    // for(let i=0; i <= userNumber; i++){
-    //     $('.results-img').replaceWith(`<img src="${responseJson[i].message}"class="results-img">`)
-    // }
-    // // remove hidden view class/Display results
-    //     $('.dog-photo').removeClass(`hidden`)
-    // }
-
-    // reassign submit button to pull random image
- function watchForm() {
-  $('.choose-number-form').submit(event => {
-    event.preventDefault();
-    watchNumberInput();
-  });
+function listenToInput() {
+ $('.js-search-form').submit(event => {
+   event.preventDefault();
+   const queryTarget = $(event.currentTarget).find('.js-query');
+   const query = queryTarget.val();
+    queryTarget.val("3")
+   getImages(query, searchData);
+ });
 }
 
 
 $(function() {
-  console.log('App loaded! Waiting for submit!');
-  watchForm();
-});
+ console.log('App loaded! Waiting for submit!');
+ listenToInput();
+}); 
 
-
-function displayResults(responseJson) {
-  console.log(responseJson);
-  //replace the existing image with the new one
-  return `<div>
-    <img src="${responseJson}" class="results-img"></div>
-  </div>`
-
-}
-
-function displayDogSearchData(data) {
-  const results = data.message.map((item, index) => displayResults(item));
-  $('.js-results').html(results); 
-  
-  //display the results section
-  $('.dog-photo').removeClass('hidden');
-}
